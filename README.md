48fd340ae350b737e7eb8f1003720a0c98b5adf1
Himanshu <sharma.himanshu13391@gmail.com>
2021-05-08 00:46:52 +0530

    State Mgmt | Parent maintaing child state | Mix Approach

--- README.md
+++ README.md
@@ -2,15 +2,20 @@
 
 A new Flutter application.
 
-## Demo 5
+## Demo 6
 
 The objective of this demo is to:
-- Create a custom widget.
-- Changing state of widget. On click of rating icon it toggles is state and count changes.
+- Parent Widget manges child widget state
+- Mix Match Approch: Stateful widget manges some of the states, and parent widget manges other aspects of the state
+
+The TapBox state manage:
+* On tap the color changes to Green or Grey. State is maintained by parent
+* Once tap is down a border is shown, on up it disappears. State is maintained by widget itself i.e child
 
 #### App design ####
-![picture alt](https://raw.githubusercontent.com/HimanshuSharma13/Flutter/feature_d5_stateful_widget/screens/home1.png "App design screens")
-![picture alt](https://raw.githubusercontent.com/HimanshuSharma13/Flutter/feature_d5_stateful_widget/screens/home2.png "App design screens")
+![picture alt](https://raw.githubusercontent.com/HimanshuSharma13/Flutter/feature_d6_state_mgmt/screens/home1.png "App design screens")
+![picture alt](https://raw.githubusercontent.com/HimanshuSharma13/Flutter/feature_d6_state_mgmt/screens/home2.png "App design screens")
+![picture alt](https://raw.githubusercontent.com/HimanshuSharma13/Flutter/feature_d6_state_mgmt/screens/home3.png "App design screens")
 
 
 A few resources to get you started if this is your first Flutter project:
--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'dart:developer';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-
-
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-          /*1*/
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /*2*/
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Oeschinen Lake Campground',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Text(
-                'Kandersteg, Switzerland',
-                style: TextStyle(
-                  color: Colors.grey[500],
-                ),
-              ),
-            ],
-          ),
-        ),
-        /*3*/
-
-         FavoriteWidget()
-
-      ],
-    ),
-  );
-
-  static Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  static Color color = Colors.blue[700];
-
-  Widget buttonSection = Container(
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-        _buildButtonColumn(color, Icons.share, 'SHARE'),
-      ],
-    ),
-  );
-
-  Widget textSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Text(
-      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-          'Alps. Situated 1,578 meters above sea level, it is one of the '
-          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-          'half-hour walk through pastures and pine forest, leads you to the '
-          'lake, which warms to 20 degrees Celsius in the summer. Activities '
-          'enjoyed here include rowing, and riding the summer toboggan run.'
-          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-          'Alps. Situated 1,578 meters above sea level, it is one of the '
-          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-          'half-hour walk through pastures and pine forest, leads you to the '
-          'lake, which warms to 20 degrees Celsius in the summer. Activities '
-          'enjoyed here include rowing, and riding the summer toboggan run.',
-      softWrap: true,
-    ),
-  );
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    log('MyApp build');
     return MaterialApp(
         title: 'Flutter Layout Demo',
         theme: ThemeData(
@@ -113,73 +22,117 @@ class MyApp extends StatelessWidget {
           ),
           body: ListView(
             children: [
-
               Image.asset(
-                  'images/mainimage.jpg',
-
-                  fit: BoxFit.cover,
-                  ),
-              titleSection,
-              buttonSection,
-              textSection
+                'images/mainimage.jpg',
+                fit: BoxFit.cover,
+              ),
+              _ParentWidget()
             ],
           ),
         ));
   }
-
-
 }
 
-class FavoriteWidget extends StatefulWidget{
-
-
+class _ParentWidget extends StatefulWidget {
   @override
-  _FavoriteWidgetState createState() {
+  _ParentWidgetState createState() {
     // TODO: implement createState
-    return _FavoriteWidgetState();
+    return _ParentWidgetState();
   }
-
 }
 
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = true;
+class _ParentWidgetState extends State<_ParentWidget> {
+  bool _active = false;
   int _favoriteCount = 42;
 
-  void _toggeleFavorite(){
-    setState(() {
-      if(_isFavorited){
-        _isFavorited=false;
-        _favoriteCount=_favoriteCount-1;
-      }else{
-        _isFavorited=true;
-        _favoriteCount=_favoriteCount+1;
-      }
-    });
-}
+  void _handleTapboxChanged(bool newValue) => {
+        setState(() {
+          _active = newValue;
+        })
+      };
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            color: Colors.red[500],
-            padding: EdgeInsets.all(0),
-            icon: _isFavorited?Icon(Icons.star):Icon(Icons.star_border),
-            onPressed: _toggeleFavorite,
-          ),
+    log('_ParentWidgetState build');
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: ChildWidget(
+          _active,
+          _handleTapboxChanged,
         ),
-        SizedBox(
-          width: 18,
-          child: Container(
-            child: Text(_favoriteCount.toString()),
-          ),
-        )
+        // child: ChildWidget(this.context, _active, _handleTapboxChanged(_active)),
+      ),
 
-      ],
     );
+  }
+}
+
+class ChildWidget extends StatefulWidget {
+  bool active;
+  final ValueChanged<bool> onChanged;
+
+  ChildWidget(this.active, this.onChanged);
+
+
+
+
+
+
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return ChildState();
+  }
+}
+
+class ChildState extends State<ChildWidget> {
+  bool highlight=false;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    log('ChildWidget build');
+    return GestureDetector(
+      child: Container(
+        child: Center(
+          child: Text(widget.active ? 'Open' : 'Closed',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: highlight
+              ? Border.all(
+            color: Colors.teal[700],
+            width: 10.0,
+          )
+              : null,
+        ),
+      ),
+      onTap: _handleTap,
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+    );
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      highlight=true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      highlight=false;
+    });
   }
 }
