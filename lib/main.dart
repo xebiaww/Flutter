efce2bd10020f2d2b3901db2231bcbab30ae3bef
Himanshu <sharma.himanshu13391@gmail.com>
2021-06-16 15:09:57 +0530

    Getting android battery level

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/rendering.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -20,28 +20,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'URL Launch Demo',
+      title: 'Battery Level Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('URL Launch Demo'),
+          title: Text('Battery Level Demo'),
         ),
         body: Container(
           alignment: Alignment.center,
           child: DefaultTextStyle(
-
             style: descTextStyle,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('Launch Flutter Site'),
+                Text('Get Battery Level'),
                 Container(
                   width: 300,
                   height: 100,
                   margin: EdgeInsets.all(30),
-                  child: NativeButton(),
+                  child: BatteryFrame(),
                 )
               ],
             ),
@@ -52,36 +51,43 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-class NativeButton extends StatefulWidget {
+class BatteryFrame extends StatefulWidget {
   @override
-  _NativeButtonState createState() => _NativeButtonState();
+  _BatteryFrameState createState() => _BatteryFrameState();
 }
 
-class _NativeButtonState extends State<NativeButton> {
-
+class _BatteryFrameState extends State<BatteryFrame> {
+  String _batteryLevel = '(Unknown battery level)';
+  static const batteryPlatform = const MethodChannel('hs.flutter.demo/battery');
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ElevatedButton(
-        onPressed: _launchURL,
-        child: Text(
-          'Launch!!',
-          style: TextStyle(fontSize: 20),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        ElevatedButton(
+          onPressed: _getBatteryLevel,
+          child: Text(
+            'GET',
+            style: TextStyle(fontSize: 20),
+          ),
         ),
-      ),
+        Text(_batteryLevel)
+      ]),
     );
   }
 
-  _launchURL() async {
-    const url = 'https://flutter.io';
-    // if (await canLaunch(url)) {
-      await launch(url);
-    // } else {
-    //   throw 'Could not launch $url';
-    // }
+  _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result =
+          await batteryPlatform.invokeMethod('getPlatformBatteryLevel');
+      batteryLevel = '(Battery Level at $result %)';
+    } on PlatformException catch (e) {
+      batteryLevel = '(Failed to get Battery Level: ${e.message})';
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
   }
 }
-
